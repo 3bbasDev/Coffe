b366b782533aa90d5c83b27fe31a7875c2075e79
Abbas K. Radam <abbasabbe1995@gmail.com>
2022-07-05 18:14:20 +0300

    up

---
+++ Coffe/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.Entities
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FullName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public Guid UserTypeId { get; set; }//fk
+        public UserType UserType { get; set; }
+    }
+
+}
