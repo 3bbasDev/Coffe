ae3c2e6024ab58ff0bdbc4dd7386db6a5eb6b396
Abbas K. Radam <abbasabbe1995@gmail.com>
2022-07-08 12:31:49 +0300

    add entities

---
+++ Coffe/Entities/Address.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Coffe.Entities
+{
+    public class Address
+    {
+        public Guid Id { get; set; }
+        public double Long { get; set; }
+        public double Lat { get; set; }
+        public string? Stret { get; set; }
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+        public ICollection<Order>? Orders { get; set; }
+    }
+}
--- Coffe/Entities/UserType.cs
+++ Coffe/Entities/UserType.cs
@@ -10,7 +10,7 @@ namespace Coffe.Entities
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
-        public ICollection<User> Users { get; set; }//a
+        public ICollection<User> Users { get; set; }
     }
 
 }
