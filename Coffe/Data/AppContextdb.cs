bc9079bc762053b05468f735c826668b1cac300e
Abbas K. Radam <abbasabbe1995@gmail.com>
2022-07-08 14:04:37 +0300

    add  dbconfig

---
+++ Coffe/Data/AppContextdb.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Coffe.Entities;
+
+namespace Coffe.Data
+{
+    public class AppContextdb : DbContext
+    {
+        public AppContextdb(DbContextOptions options) : base(options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+        }
+
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrederItem> OrederItems { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public DbSet<UserType> UserTypes { get; set; }
+    }
+}
