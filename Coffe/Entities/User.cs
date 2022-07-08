f4b21113aa8ab92cdd5261b7dbd5858cc2976813
Abbas K. Radam <abbasabbe1995@gmail.com>
2022-07-08 13:39:49 +0300

    add db conf user

--- Coffe/Entities/User.cs
+++ Coffe/Entities/User.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,6 +18,21 @@ namespace Coffe.Entities
         public UserType? UserType { get; set; }
         public ICollection<Item>? Items { get; set; }
         public ICollection<Address>? Addresses { get; set; }
+    }
+    public class UserConfigurations : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.Property(f => f.Id).ValueGeneratedOnAdd().IsRequired();
+            builder.Property(f => f.FullName).IsRequired().HasMaxLength(100);
+            builder.Property(f => f.UserName).IsRequired().HasMaxLength(50);
+            builder.Property(f => f.Password).IsRequired().HasMaxLength(50);
+            builder.Property(f => f.UserTypeId).IsRequired();
+
+            builder.HasOne(f => f.UserType)
+                .WithMany(f => f.Users)
+                .HasForeignKey(f => f.UserTypeId);
+        }
     }
 
 }
