86433f10979299ebc9cea30dda0dfb1f5b5ce1dc
Aqeel Jabbar <37226404+aqeel97@users.noreply.github.com>
2022-07-30 17:15:31 +0300

    Add files via upload
    update

--- Coffe/Program.cs
+++ Coffe/Program.cs
@@ -1,22 +1,52 @@
 using Coffe.Data;
+using Coffe.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
-
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddDbContext<AppContextdb>(options =>
 {
     options.UseNpgsql(builder.Configuration["ConnectionStrings:DefaultConnection"], options =>
-        {
-            options.CommandTimeout(30);
-            options.EnableRetryOnFailure(3);
-        });
+    {
+        options.CommandTimeout(30);
+        options.EnableRetryOnFailure(3);
+    });
 });
+
+
+builder.Services.AddControllers()
+                .AddNewtonsoftJson(options =>
+                {
+                    options.SerializerSettings.Converters.Add(new StringEnumConverter(new CamelCaseNamingStrategy()));
+                    options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
+                });
+
+
+builder.Services.AddScoped<IAccountRepository, AccountRepository>();
+builder.Services.AddScoped<IAddressRepository, AddressRepository>();
+builder.Services.AddMvc();
+builder.Services.AddAutoMapper(typeof(Program));
+
 var app = builder.Build();
-app.MapGet("/", () => "Hello World!");
+app.UseRouting();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
+
+
+
+
+
+
+
+
